ab281cf781e2576891777bb7854bfe978287b6cf
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-02-10 22:55:04 -0500

    Add notFoundRoute to router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import {
+  RouterProvider,
+  createRouter,
+  NotFoundRoute,
+} from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen';
+import { routeTree } from "./routeTree.gen";
+import { Route as rootRoute } from "./routes/__root.tsx";
+
+const notFoundRoute = new NotFoundRoute({
+  getParentRoute: () => rootRoute,
+  component: () => "404 not found",
+});
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, notFoundRoute });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
